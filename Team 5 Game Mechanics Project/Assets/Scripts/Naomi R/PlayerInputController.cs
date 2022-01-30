b0c1d6ff87371e171b606867e57ca8c9d8ee90d9
GREM <naomiruokamo@gmail.com>
2022-01-30 22:09:50 +0100

    Update PlayerInputController.cs

--- Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/PlayerInputController.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/PlayerInputController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerInputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float MoveInput { get; private set; }
+    public bool JumpInputDown { get; private set; }
+    public bool JumpInputUp { get; private set; }
+    public bool JumpInput { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MoveInput = Input.GetAxis("Horizontal");
+        JumpInputDown = Input.GetKeyDown(KeyCode.Space);
+        JumpInputUp = Input.GetKeyUp(KeyCode.Space);
+        JumpInput = Input.GetKey(KeyCode.Space);
     }
 }
