098f46bba5555c1d4a9cdff71b785689596b8510
GREM <naomiruokamo@gmail.com>
2022-01-30 22:09:52 +0100

    Update PlayerWalkController.cs

--- Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/PlayerWalkController.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/PlayerWalkController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerWalkController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody myRigidbody;
+    private PlayerInputController playerInputController; 
+    private GroundChecker groundChecker;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float chargingMoveSpeedFactor = 0.5f;
+    
+    private void Awake()
     {
-        
+        myRigidbody = this.gameObject.GetComponent<Rigidbody>();
+        playerInputController = this.gameObject.GetComponent<PlayerInputController>();
+        groundChecker = this.gameObject.GetComponent<GroundChecker>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Slower move speed while charging a jump.
+        var currentMoveSpeed = moveSpeed;
+        if (playerInputController.JumpInput && groundChecker.IsGrounded)
+            currentMoveSpeed *= chargingMoveSpeedFactor;
+
+        myRigidbody.velocity = new Vector3(playerInputController.MoveInput * currentMoveSpeed, myRigidbody.velocity.y, 0);
     }
 }
