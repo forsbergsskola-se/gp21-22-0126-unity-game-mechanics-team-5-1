1b5d6d1e4a749b935e42988df0402f3f2a4e36ac
GREM <naomiruokamo@gmail.com>
2022-01-31 20:15:40 +0100

    Update FlightPowerUp.cs
    testing to see that it works correctly with the player collider

--- Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/FlightPowerUp.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/FlightPowerUp.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FlightPowerUp : MonoBehaviour
 {
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("power up was picked up");
+        }
+        Destroy(this.gameObject);
+    }
 }
