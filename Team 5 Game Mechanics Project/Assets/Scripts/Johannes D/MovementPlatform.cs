f7b796dd0255fa5280c2d47b8d2263e27b3e383a
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-01-31 15:32:40 +0100

    MovementPlattforms.cs
    Made it so the plattform are not slippery for the player

---
+++ Team 5 Game Mechanics Project/Assets/Scripts/Johannes D/MovementPlatform.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlatform : MonoBehaviour
+{
+   public GameObject Player;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject== Player)
+      {
+         Player.transform.parent = transform;
+      }
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      if (other.gameObject== Player)
+      {
+         Player.transform.parent = null;
+      }
+      
+   }
+}
