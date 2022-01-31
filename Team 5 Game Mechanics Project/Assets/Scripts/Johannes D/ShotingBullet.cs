f71bc4d0db7332b541ef924bc25ba68fd45c0829
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-01-31 14:44:45 +0100

    ShootingBullet Script in Early Production
    Put out a Spawner where it shoots bullets from and the bullets are in Johannes Folder

---
+++ Team 5 Game Mechanics Project/Assets/Scripts/Johannes D/ShotingBullet.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotingBullet : MonoBehaviour
+{
+    public GameObject Bullet;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ShootingTheBullet();
+        }
+    }
+
+    void ShootingTheBullet()
+    {
+       Instantiate(Bullet, new Vector3(transform.position.x+1f, transform.position.y), transform.rotation);
+         
+    }
+
+   
+}
