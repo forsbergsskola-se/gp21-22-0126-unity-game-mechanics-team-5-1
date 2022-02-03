8ae08f94ecc041da64745fb93c2bda6ec37f8f55
GREM <naomiruokamo@gmail.com>
2022-02-03 11:18:37 +0100

    Update FlightPowerUp.cs
    Tried things out, however right now the player just sinks and can't  fly up nor down.

--- Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/Flight/FlightPowerUp.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/Flight/FlightPowerUp.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 public class FlightPowerUp : MonoBehaviour
 {
     [SerializeField] private float duration = 5f;
+    [SerializeField] private float moveSpeed = 5f;
     private PlayerInputController playerInputController;
     private GameObject player;
+    private Rigidbody playerRigidbody;
 
+    
     private void Awake()
     {
         player = GameObject.FindWithTag("Player");
         playerInputController = player.GetComponent<PlayerInputController>();
+        playerRigidbody = player.GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -24,16 +28,27 @@ public class FlightPowerUp : MonoBehaviour
         }
     }
 
-    IEnumerator PowerUp(Collider player)
+    IEnumerator PowerUp(Collider collision)
     {
         Debug.Log("I started the coroutine");
 
         GetComponent<MeshRenderer>().enabled = false;
         GetComponent<BoxCollider>().enabled = false;
+        player.GetComponent<Rigidbody>().useGravity = false;
+        Flight();
         
+        Debug.Log("I started waiting");
         yield return new WaitForSeconds(duration);
         Debug.Log("I finished the waiting");
         
+        player.GetComponent<Rigidbody>().useGravity = true;
+        
         Destroy(gameObject);
+    }
+
+    private void Flight()
+    {
+        var currentMoveSpeed = moveSpeed;
+        playerRigidbody.velocity = new Vector3(playerInputController.FlightInput * currentMoveSpeed, playerRigidbody.velocity.y, 0);
     }
 }
