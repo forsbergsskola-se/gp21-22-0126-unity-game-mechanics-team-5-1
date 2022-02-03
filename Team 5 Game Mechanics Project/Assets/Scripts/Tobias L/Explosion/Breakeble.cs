7a13c25e038a918f2ceb42282fc1df4672bbe4fa
AlchemistWolf <tobias_lundin@yahoo.com>
2022-02-03 11:27:00 +0100

    breakeble_script

---
+++ Team 5 Game Mechanics Project/Assets/Scripts/Tobias L/Explosion/Breakeble.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakeble : MonoBehaviour
+{
+    [SerializeField] private GameObject _replacement;
+    [SerializeField] private float _breakForce = 2;
+    [SerializeField] private float _collisionMultiplier = 100;
+    [SerializeField] private bool _broken;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(_broken) return;
+        if (collision.relativeVelocity.magnitude >= _breakForce)
+        {
+            _broken = true;
+            var replacement = Instantiate(_replacement, transform.position, transform.rotation);
+
+            var rbs = replacement.GetComponentsInChildren<Rigidbody>();
+            foreach (var rb in rbs)
+            {
+                rb.AddExplosionForce(collision.relativeVelocity.magnitude * _collisionMultiplier,collision.contacts[0].point,2);
+            }
+            Destroy(gameObject);
+        }
+        
+    }
+}
