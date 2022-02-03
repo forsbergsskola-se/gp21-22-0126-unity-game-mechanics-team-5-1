bb1b46056ff508a9ea2ef35152e02044960ab87a
GREM <naomiruokamo@gmail.com>
2022-02-03 11:49:47 +0100

    Update FlightRegular.cs
    A functional and barebones Flight Script

--- Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/Flight/FlightRegular.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/Flight/FlightRegular.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FlightRegular : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int flightSpeed, dropSpeed;
+    private PlayerInputController playerInputController;
+    private Rigidbody playerRigidbody;
+
+    private void Awake()
     {
-        
+        playerRigidbody = this.gameObject.GetComponent<Rigidbody>();
+        playerInputController = this.gameObject.GetComponent<PlayerInputController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRigidbody.AddForce(Vector3.up *flightSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigidbody.AddForce(Vector3.down * dropSpeed);
+        }
     }
 }
