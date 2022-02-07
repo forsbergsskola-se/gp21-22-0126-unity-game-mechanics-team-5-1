799f3f8f98f4af559d2d09c4d4466c93546c6745
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-02-07 13:41:02 +0100

    Bullet script will get destroyed if it does not hit a object after it has been destroyed with bulletlife
    bullet life is a float that hold a number and it will wait for that many seconds in before it gets destroyed.

--- Team 5 Game Mechanics Project/Assets/Scripts/Johannes D/Scripts_JohannesDanielsson/Bullet_1_JohannesDanielsson.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/Johannes D/Scripts_JohannesDanielsson/Bullet_1_JohannesDanielsson.cs
@@ -6,24 +6,34 @@ using UnityEngine;
 public class Bullet_1_JohannesDanielsson : MonoBehaviour
 {
     public float bulletSpeed = 100f;
-    public float bulletLife= 3;
+    public float bulletLife= 3f;
 
+    public bool isGameobjectActive= true;
     public Rigidbody BulletRigidbody;
     // Start is called before the first frame update
     void Start()
     {
         BulletRigidbody.velocity = transform.right* bulletSpeed;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BulletGetsDestoyed();
     }
 
     private void OnCollisionEnter(Collision other)
     {
         Destroy(gameObject);
     }
+
+    private void BulletGetsDestoyed()
+    {
+        //TODO I know that the bool is not needed but I am thinking of implementing differently later on. 
+        if (isGameobjectActive)
+        {
+            Destroy(gameObject, bulletLife);
+        }
+    }
+    
 }
