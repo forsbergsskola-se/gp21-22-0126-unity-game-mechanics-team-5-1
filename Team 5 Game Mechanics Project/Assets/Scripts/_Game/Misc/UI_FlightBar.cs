6e23e0f301b77030b132687401fe0de9c3d99d04
GREM <naomiruokamo@gmail.com>
2022-02-08 21:52:55 +0100

    Update UI_FlightBar.cs
    The flight bar now decreases bar size and plays particle effects

--- Team 5 Game Mechanics Project/Assets/Scripts/_Game/Misc/UI_FlightBar.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/_Game/Misc/UI_FlightBar.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_FlightBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Slider slider;
+    private ParticleSystem particleSys;
+
+    public float speed = 0.5f;
+    private float target = 0;
+
+    void Awake()
     {
-        
+        slider = gameObject.GetComponent<Slider>();
+        particleSys = GameObject.Find("Bar Particles").GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        DecreaseBar(0.75f);
+    }
+
     void Update()
     {
-        
+        if (slider.value > target)
+        {
+            slider.value -= speed * Time.deltaTime;
+            if (!particleSys.isPlaying) particleSys.Play();
+        }
+        else particleSys.Stop();
+    }
+
+    public void DecreaseBar(float progress)
+    {
+        target = slider.value - progress;
     }
 }
