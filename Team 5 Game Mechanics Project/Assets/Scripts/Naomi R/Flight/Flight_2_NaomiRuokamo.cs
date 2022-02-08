587c5ebfdc188ced4209201155efe263d73d71a7
GREM <naomiruokamo@gmail.com>
2022-02-08 17:26:14 +0100

    Overhauling Flight_2
    The mechanic has changed to be dependent on another game object just like the power up. How they make the player fly is still different.

--- Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/Flight/Flight_2_NaomiRuokamo.cs
+++ Team 5 Game Mechanics Project/Assets/Scripts/Naomi R/Flight/Flight_2_NaomiRuokamo.cs
@@ -1,30 +1,62 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Flight_2_NaomiRuokamo : MonoBehaviour
 {
-    [SerializeField] private int flightSpeed, dropSpeed;
-    private PlayerInputController playerInputController;
+    [SerializeField] private int flightSpeed, dropSpeed, duration;
+    private GameObject player;
     private Rigidbody playerRigidbody;
+    private bool isFlying;
+
 
     private void Awake()
     {
-        playerRigidbody = this.gameObject.GetComponent<Rigidbody>();
-        playerInputController = this.gameObject.GetComponent<PlayerInputController>();
+        player = GameObject.FindWithTag("Player");
+        playerRigidbody = player.GetComponent<Rigidbody>();
+        isFlying = false;
     }
+    
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (isFlying)
         {
-            playerRigidbody.AddForce(Vector3.up * flightSpeed);
-        }
+            if (Input.GetKey(KeyCode.W))
+            {
+                playerRigidbody.AddForce(Vector3.up * flightSpeed);
+            }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            playerRigidbody.AddForce(Vector3.down * dropSpeed);
+            if (Input.GetKey(KeyCode.S))
+            {
+                playerRigidbody.AddForce(Vector3.down * dropSpeed);
+            }
         }
+    }
+    
+    
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(FlyPowerUp(collision));
+        }
+    }
+
+    IEnumerator FlyPowerUp(Collider collision)
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        
+        playerRigidbody.useGravity = false;
+
+        isFlying = true;
+        
+        yield return new WaitForSeconds(duration);
+
+        playerRigidbody.useGravity = true;
+        
+        isFlying = false;
+        
+        Destroy(gameObject);
     }
 }
